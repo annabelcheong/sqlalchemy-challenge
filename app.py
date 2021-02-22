56093e8071310a6d01fecfa4306343ceb6fee904
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-02-22 21:39:16 +0800

    progressed app.py

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 #Dependencies
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-#LIST ALL THE DICTIONARIES HERE#######
-#precip_data= [
 
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
 
-
-# ]
-
-
-############
+# Save reference to the table
+Measurement =  Base.classes.measurement
+Station = Base.classes.station
 
 #################################################
 # Flask Setup
@@ -20,38 +29,59 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-
-# @app.route("/api/v1.0/justice-league")
-# def justice_league():
-#     """Return the justice league data as json"""
-
-#     return jsonify(justice_league_members)
-
-    @app.route("/")
-def homepage():
+@app.route("/")
+def welcome():
+    """List all available api routes."""
     return (
-        f"Welcome to the Climate API!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start><end><br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
     )
 
-    @app.route("/api/v1.0/precipitation/<date>")
-def precip_data(date):
-    """Fetch the Justice League character whose real_name matches
-       the path variable supplied by the user, or a 404 if not."""
+@app.route("/")
+ def homepage():
+     return (
+         f"Welcome to the Climate API!<br/>"
+         f"Available Routes:<br/>"
+         f"/api/v1.0/precipitation<br/>"
+         f"/api/v1.0/stations<br/>"
+         f"/api/v1.0/tobs<br/>"
+         f"/api/v1.0/<start><br/>"
+         f"/api/v1.0/<start><end><br/>"
+    )
 
-    canonicalized = real_name.replace(" ", "").lower() #From API, what user enters
-    for character in justice_league_members:
-        search_term = character["real_name"].replace(" ", "").lower() #Changes letters in dictionary
+#     @app.route("/api/v1.0/precipitation/<date>")
+# def precip_data(date):
+#     """Fetch the Justice League character whose real_name matches
+#        the path variable supplied by the user, or a 404 if not."""
 
-        if search_term == canonicalized:
-            return jsonify(character)
+#     canonicalized = real_name.replace(" ", "").lower() #From API, what user enters
+#     for character in justice_league_members:
+#         search_term = character["real_name"].replace(" ", "").lower() #Changes letters in dictionary
 
-    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
+#         if search_term == canonicalized:
+#             return jsonify(character)
+
+#     return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
+
+
+
+
+
+
+
+
+#Convert the query results to a dictionary using date as the key and prcp as the value.
+# Measurement_yr = session.query(Measurement.date, Measurement.prcp).\
+#     filter(Measurement.date >= query_date).\
+#     order_by(Measurement.date).\
+#     all()
+#print(Measurement_yr)
+
+
+# # Create our session (link) from Python to the DB
+# session = Session(engine)
+# ]
 
 
 if __name__ == "__main__":
