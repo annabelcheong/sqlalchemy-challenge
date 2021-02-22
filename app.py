1d16133ada3e49866f7a0b9efc57dc617432c0b4
annabelcheong <73626102+annabelcheong@users.noreply.github.com>
2021-02-22 23:21:44 +0800

    Completed date, precip json and distinct station json

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from sqlalchemy import distinct
 from flask import Flask, jsonify
 
 
@@ -29,17 +30,9 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+# Homepage
 @app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
-    )
-
-@app.route("/")
- def homepage():
+def homepage():
      return (
          f"Welcome to the Climate API!<br/>"
          f"Available Routes:<br/>"
@@ -49,20 +42,111 @@ def welcome():
          f"/api/v1.0/<start><br/>"
          f"/api/v1.0/<start><end><br/>"
     )
+########
 
-#     @app.route("/api/v1.0/precipitation/<date>")
-# def precip_data(date):
-#     """Fetch the Justice League character whose real_name matches
+# Precipitation Page
+# Return a list of Date and Precipitation. 
+# Convert each sub-list to dictionaries. 
+@app.route("/api/v1.0/precipitation/")
+def Measurement_dict():
+
+###########################################################
+## QUERY ##
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query 
+    Measurement_qry = session.query(Measurement.date, Measurement.prcp).\
+        order_by(Measurement.date).\
+        all()
+
+    session.close()
+###########################################################
+## CONVERSION TO DICTIONARIES ##
+
+# Create a dictionary from the row data and append to a list of all_passengers
+    Measurements_all = []
+    for date, prcp in Measurement_qry:
+        Measurement_dict = {}
+        Measurement_dict["date"] = date
+        Measurement_dict["prcp"] = prcp
+        Measurements_all.append(Measurement_dict)
+
+    return jsonify(Measurements_all)
+
+######
+######
+#Stations Page
+@app.route("/api/v1.0/stations/")
+def Stations_list():
+###########################################################
+## QUERY ##
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of Date and Precipitation. Name dictionary as Measurement_dict"""
+    # Query 
+    distinct_stations_qry = session.query(distinct(Measurement.station)).all()
+
+    session.close()
+
+#############################################################
+    return jsonify(distinct_stations_qry)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     #User entry in the website for the date in 
+#     user_input = date.replace(" ", "")
+#     # for d in Measurement_dict:
+#     #     return jsonify(d)
+        
+
+# @app.route("/api/v1.0/justice-league/justice-league/superhero/<superhero>")
+# def justice_league_character(superhero):
+#     """Fetch the Justice League character whose superhero matches
 #        the path variable supplied by the user, or a 404 if not."""
 
-#     canonicalized = real_name.replace(" ", "").lower() #From API, what user enters
+#     canonicalized = superhero.replace(" ", "").lower() #From API, what user enters in the website
 #     for character in justice_league_members:
-#         search_term = character["real_name"].replace(" ", "").lower() #Changes letters in dictionary
+#         search_term = character["superhero"].replace(" ", "").lower() #Changes letters in dictionary
 
 #         if search_term == canonicalized:
 #             return jsonify(character)
 
-#     return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
+#     return jsonify({"error": f"Character with superhero {superhero} not found."}), 404
+
+
+
+#     # Convert list of tuples into normal list
+#     # Measurement_list = list(np.ravel(Measurement_qry))
+
+#     return jsonify(Measurement_dict)
+
+# #     """Fetch the Justice League character whose real_name matches
+# #        the path variable supplied by the user, or a 404 if not."""
+
+# #     canonicalized = real_name.replace(" ", "").lower() #From API, what user enters
+# #     for character in justice_league_members:
+# #         search_term = character["real_name"].replace(" ", "").lower() #Changes letters in dictionary
+
+# #         if search_term == canonicalized:
+# #             return jsonify(character)
+
+# #     return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
 
 
 
@@ -71,17 +155,17 @@ def welcome():
 
 
 
-#Convert the query results to a dictionary using date as the key and prcp as the value.
-# Measurement_yr = session.query(Measurement.date, Measurement.prcp).\
-#     filter(Measurement.date >= query_date).\
-#     order_by(Measurement.date).\
-#     all()
-#print(Measurement_yr)
+# #Convert the query results to a dictionary using date as the key and prcp as the value.
+# # Measurement_yr = session.query(Measurement.date, Measurement.prcp).\
+# #     filter(Measurement.date >= query_date).\
+# #     order_by(Measurement.date).\
+# #     all()
+# #print(Measurement_yr)
 
 
-# # Create our session (link) from Python to the DB
-# session = Session(engine)
-# ]
+# # # Create our session (link) from Python to the DB
+# # session = Session(engine)
+# # ]
 
 
 if __name__ == "__main__":
